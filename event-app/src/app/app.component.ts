3c699de2746f175e303974fbc87279f7df356c43
Julian <husson.j@yahoo.com>
2019-11-13 12:00:53 +0100

    récuperation des push et artistes

--- event-app/src/app/app.component.ts
+++ event-app/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { EventModel } from './global/event.model';
+import { PushModel } from './global/push.model';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'event-app';
+  alert = true;
+
+  pushs: PushModel[] = [];
+  events: EventModel[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get<[]>('https://mspr.chloe-gautreau.com/wp-json/wp/v2/push').subscribe(resPushs => {
+      if(resPushs){
+        resPushs.forEach(push => {
+          const name = push.acf.push_name; 
+          const description = push.acf.push_description;
+          this.pushs.push({name, description});
+        });
+      }
+    });
+    this.http.get<[]>('https://mspr.chloe-gautreau.com/wp-json/wp/v2/event').subscribe(resEvents => {
+      if(resEvents){
+        resEvents.forEach(event => {
+          const name = event.acf.artiste_name;
+          const description = event.acf.artiste_description;
+          const image = event.acf.artiste_image;
+          const start_date = event.acf.artiste_start_date;
+          const end_date = event.acf.artiste_end_date;
+          const stage = event.acf.stage_name;
+          this.events.push({name, description, image, start_date, end_date, stage});
+        });
+      }
+    })
+  }
+
+  close(push) {
+    this.pushs.splice(this.pushs.indexOf(push), 1);
+  }
+
 }
---
+++ event-app/src/app/global/event.model.ts
@@ -0,0 +1,8 @@
+export class EventModel {
+    name: string;
+    description: string;
+    image: string;
+    start_date: Date;
+    end_date: Date;
+    stage: string;
+}
